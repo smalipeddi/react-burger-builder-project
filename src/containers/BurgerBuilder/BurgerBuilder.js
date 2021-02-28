3d15c8af3812ae77f5b29936c4c4333d4566ca9e
Sunitha Malipeddi <sunitha_malipeddi@yahoo.com>
2021-02-28 01:23:50 -0500

    Initial react burger builder

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,25 @@
+import React , {Component} from 'react';
+import Aux from "../../hoc/Aux";
+import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+
+class BurgerBuilder extends Component {
+
+	state = {
+		ingredients: {
+			salad: 1,
+			bacon: 2,
+			cheese: 1,
+			meat: 1
+		}
+	}
+
+	render() {
+		return ( 
+				<Aux>
+					<Burger ingredients={this.state.ingredients} />
+					<BuildControls />
+				</Aux>
+				)}
+}
+export default BurgerBuilder;
